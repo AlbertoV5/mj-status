235e6e0a9a6a1ede7c03126722cd9d1eee82dec7
AlbertoV5 <58243333+AlbertoV5@users.noreply.github.com>
2023-05-15 23:38:53 -0600

    added plot :)

--- src/scripts/stats.ts
+++ src/scripts/stats.ts
@@ -1,44 +1,25 @@
 import * as d3 from "d3";
 
-
-function convertToHourMinutes(minutesList: number[]) {
-    var result: string[] = [];
-
-    minutesList.forEach(function (minutes) {
-        var hours = Math.floor(minutes / 60); // Calculate the hours
-        var mins = minutes % 60; // Calculate the remaining minutes
-
-        // Format the hours and minutes
-        var formattedTime = ("0" + hours).slice(-2) + ":" + ("0" + mins).slice(-2);
-
-        result.push(formattedTime); // Add the formatted time to the result array
-    });
-
-    return result;
-}
-
+// Data Plot
 const url = "../metrics/relax/2023-05-14_2023-05-15.json";
-
+const elementId = "#test";
+// TODO: Finish implementation
 const margin = {top: 10, right: 30, bottom: 30, left: 60},
 width = 900 - margin.left - margin.right,
 height = 400 - margin.top - margin.bottom;
-// append the svg object to the body of the page
-const svg = d3.select("#test")
+// Append the svg object to the body of the page
+const svg = d3.select(elementId)
 .append("svg")
     .attr("width", width + margin.left + margin.right)
     .attr("height", height + margin.top + margin.bottom)
 .append("g")
     .attr("transform", `translate(${margin.left},${margin.top})`);
-
+// Keys
 type key = "kdpt_diffusion_anime" | "v4_anime_upscaler" | "v4_diffusion" | "v4_upscaler" | "v5_diffusion" | "v5_diffusion_anime";
-
-// const X = convertToHourMinutes([...Array(96).keys()].map(x => x * 15));
-const X = [...Array(96).keys()].map(x => x * 15);
-console.log(X);
+const testKey = 'v5_diffusion_anime';
 //Read the data
-// v5_diffusion_anime
 (d3.json(url) as Promise<Record<key, number[]>>)
-.then((data: Record<key, number[]>) => (data['v5_diffusion_anime']))
+.then((data: Record<key, number[]>) => (data[testKey]))
 .then((values: number[]) => {
     // time
     const utc = new Date().getTimezoneOffset() * 60000;
@@ -46,7 +27,7 @@ console.log(X);
     const yesterday = today - 24 * 60 * 60 * 1000;
     const minutes15 = 15 * 60 * 1000;
     // data
-    const data = values.map((value, i) => ({x: yesterday + (i * minutes15), y: value}));
+    const data = values.map((value, i) => ([yesterday + (i * minutes15), value]))
     // Create X axis
     const xAxis = d3.scaleTime()
     .domain([yesterday, today])
@@ -54,10 +35,10 @@ console.log(X);
     // Add X axis
     svg.append("g")
     .attr("transform", "translate(0," + height + ")")
-    .call(d3.axisBottom(xAxis).ticks(12).tickFormat(d3.timeFormat("%H:%M") as any))
+    .call(d3.axisBottom(xAxis).tickFormat(d3.timeFormat("%H:%M") as any))
     // Create Y axis
     const yAxis = d3.scaleLinear()
-    .domain( [Math.min(...values), Math.max(...values)] )
+    .domain( [0, Math.max(...values)] )
     .range([ height, 0 ]);
     // Add Y axis
     svg.append("g")
@@ -68,7 +49,7 @@ console.log(X);
     .attr("fill", "none")
     .attr("stroke", "#69b3a2")
     .attr("stroke-width", 1.5)
-    .attr("d", d3.line().x(d => xAxis(d.x)).y(d => yAxis(d.y)) as any
+    .attr("d", d3.line().x(([x, y]) => xAxis(x)).y(([x, y]) => yAxis(y)) as any
     )
     // Add the points
     svg
@@ -76,10 +57,8 @@ console.log(X);
     .selectAll("dot")
     .data(data)
     .join("circle")
-        .attr("cx", d => xAxis(d.x))
-        .attr("cy", d => yAxis(d.y))
+        .attr("cx", ([x, y]) => xAxis(x))
+        .attr("cy", ([x, y]) => yAxis(y))
         .attr("r", 5)
         .attr("fill", "#69b3a2")
 })
-const testElement = document.getElementById('test');
-const testData = '';
