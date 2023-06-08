c6715a3b77948b0bc32cde91fcf970bf2ff75b58
AlbertoV5 <avq5ac1@gmail.com>
2023-06-08 18:17:54 -0500

    improvements

--- src/components/global/ProjectCard.tsx
+++ src/components/global/ProjectCard.tsx
@@ -14,18 +14,19 @@ export const ProjectCard = ({date, title, tags, summary, link, thumbnail}: Proje
         window.open(link)
     }
     return (
-        <li onClick={handleClick} className="card text-bg-dark shadow-sm hstack gap-2 project-card">
+        <section onClick={handleClick} className="card text-bg-dark shadow-sm hstack gap-2 project-card">
             <section className="card-body col-6">
                 <a href={link} style={{textDecoration: "none"}}>
                     <h3 className="card-title">{title}</h3>
                 </a>
                 <p className="card-text">{summary}</p>
+                <p className='form-text'>{date.toDateString()}</p>
             </section>
             <section className='col-6'>
                 <a href={link}>
                     <img src={thumbnail} className="card-img-top" style={{width: "100%"}} alt={title}/>
                 </a>
             </section>
-        </li>
+        </section>
     )
 }
