ae943318fa4d78b984edc3791a92767a2db2aaed
AlbertoV5 <avq5ac1@gmail.com>
2023-06-09 13:07:59 -0600

    improved collection index

--- src/components/global/EntryCard.tsx
+++ src/components/global/EntryCard.tsx
@@ -1,32 +1,29 @@
 import type React from 'react'
+import type { EntryData } from '../../content/config'
 
-interface ProjectCardProps {
-    date: Date
-    title: string
-    tags: string[]
-    summary: string
-    link: string
-    thumbnail?: string
-    children: React.ReactNode
+export interface EntryCardProps {
+    data: EntryData
+    slug: string
+    children?: React.ReactNode
 }
 
-export const ProjectCard = ({date, title, tags, summary, link, thumbnail, children}: ProjectCardProps) => {
+export const EntryCard = ({data, slug, children=null}: EntryCardProps) => {
     return (
-        <a href={link} style={{textDecoration: "none"}}>
+        <a href={slug} style={{textDecoration: "none"}}>
             <section className="card text-bg-dark shadow hstack gap-2">
                 <section
                     className='col-6'
                     style={{width: "200px", height: "200px", overflow: "hidden"}}
                 >
-                    <img src={thumbnail} className="card-img-top" alt={title}/>
+                    <img src={data.thumbnail} className="card-img-top" alt={data.title}/>
                     {/* {children} */}
                 </section>
                 <section className="card-body col-6 d-flex flex-column justify-content-center">
-                    <h3 className="card-title project-card-title">{title}</h3>
+                    <h3 className="card-title project-card-title">{data.title}</h3>
                     <p className='form-text pb-2' style={{lineHeight: "0.5rem"}}>
-                        {`Date: ${date.toDateString()}`}
+                        {`Date: ${data.date.toDateString()}`}
                     </p>
-                    <p className="card-text pb-2">{summary}</p>
+                    <p className="card-text pb-2">{data.summary}</p>
                     {/* <p className='form-text pb-1' style={{lineHeight: "0.4rem"}}>
                         {`Tags: ${tags.join(", ")}`}
                     </p> */}
