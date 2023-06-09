ed339579006511d9e0c60c6dd7badbf0308fc4ea
AlbertoV5 <avq5ac1@gmail.com>
2023-06-09 13:13:06 -0600

    slug fix

--- src/components/global/CollectionIndex.tsx
+++ src/components/global/CollectionIndex.tsx
@@ -5,43 +5,44 @@ interface CollectionIndexProps {
     title: string
     description: string
     entries: Entry[]
+	slugBase: string
 }
 
-const CollectionIndex = ({title, description, entries}: CollectionIndexProps) => {
+const CollectionIndex = ({title, description, entries, slugBase}: CollectionIndexProps) => {
     return (
         <>
-        <section className="row pt-4">
-			<section className="col-md-12">
-				<header className="vstack gap-3 text-center">
-					<section className="hstack gap-2 d-flex justify-content-center">
-						<h1>{title}</h1>
-					</section>
-					<p>{description}</p>
-					<section className="hstack gap-2 d-flex justify-content-center">
-					</section>
-				</header>
+			<section className="row pt-4">
+				<section className="col-md-12">
+					<header className="vstack gap-3 text-center">
+						<section className="hstack gap-2 d-flex justify-content-center">
+							<h1>{title}</h1>
+						</section>
+						<p>{description}</p>
+						<section className="hstack gap-2 d-flex justify-content-center">
+						</section>
+					</header>
+				</section>
 			</section>
-		</section>
-		<section className="row pt-2">
-			<aside className="col-md-2"></aside>
-			<section className="col-md-8">
-				<ul className="list-group">
-					{entries.map(entry => (
-						<li 
-							key={entry.slug}
-							className="list-group-item list-group-item-dark border-0 project-wrapper" 
-							style={{padding: "1px"}}
-						>
-							<EntryCard
-								data={entry.data}
-								slug={`articles/${entry.slug}`}
-							/>
-						</li>
-					))}
-				</ul>
+			<section className="row pt-2">
+				<aside className="col-md-2"></aside>
+				<section className="col-md-8">
+					<ul className="list-group">
+						{entries.map(entry => (
+							<li 
+								key={entry.slug}
+								className="list-group-item list-group-item-dark border-0 project-wrapper" 
+								style={{padding: "1px"}}
+							>
+								<EntryCard
+									data={entry.data}
+									slug={`${slugBase}/${entry.slug}`}
+								/>
+							</li>
+						))}
+					</ul>
+				</section>
+				<aside className="col-md-2"></aside>
 			</section>
-			<aside className="col-md-2"></aside>
-		</section>
         </>
     )
 }
