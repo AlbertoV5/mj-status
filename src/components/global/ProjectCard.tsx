8133b046fba8a879a5a3026547716d51dc8d8a12
AlbertoV5 <avq5ac1@gmail.com>
2023-06-08 20:03:28 -0600

    projects

--- src/components/global/ProjectCard.tsx
+++ src/components/global/ProjectCard.tsx
@@ -10,23 +10,26 @@ interface ProjectCardProps {
 }
 
 export const ProjectCard = ({date, title, tags, summary, link, thumbnail}: ProjectCardProps) => {
-    const handleClick = () => {
-        window.open(link)
-    }
     return (
-        <section onClick={handleClick} className="card text-bg-dark shadow-sm hstack gap-2 project-card">
-            <section className="card-body col-6">
-                <a href={link} style={{textDecoration: "none"}}>
-                    <h3 className="card-title">{title}</h3>
-                </a>
-                <p className="card-text">{summary}</p>
-                <p className='form-text'>{date.toDateString()}</p>
+        <a href={link} style={{textDecoration: "none"}}>
+            <section className="card text-bg-dark shadow hstack gap-2">
+                <section
+                    className='col-6'
+                    style={{width: "200px", height: "200px", overflow: "hidden"}}
+                >
+                    <img src={thumbnail} className="card-img-top" alt={title}/>
+                </section>
+                <section className="card-body col-6 d-flex flex-column justify-content-center">
+                    <h3 className="card-title project-card-title">{title}</h3>
+                    <p className='form-text pb-2' style={{lineHeight: "0.5rem"}}>
+                        {`Date: ${date.toDateString()}`}
+                    </p>
+                    <p className="card-text pb-2">{summary}</p>
+                    {/* <p className='form-text pb-1' style={{lineHeight: "0.4rem"}}>
+                        {`Tags: ${tags.join(", ")}`}
+                    </p> */}
+                </section>
             </section>
-            <section className='col-6'>
-                <a href={link}>
-                    <img src={thumbnail} className="card-img-top" style={{width: "100%"}} alt={title}/>
-                </a>
-            </section>
-        </section>
+        </a>
     )
 }
