90b7a88a1e27878490286dfa4ebf23ec520ba9fc
AlbertoV5 <avq5ac1@gmail.com>
2023-06-10 12:11:42 -0600

    small change

--- src/components/global/Buttons.tsx
+++ src/components/global/Buttons.tsx
@@ -24,33 +24,13 @@ export const ArticlesButton = ({dark=false}: {dark?: boolean}) => {
 
 export const ThemeButton = () => {
     return (
-        // <button
-        //     id="bd-theme"
-        //     type="button"
-        //     className="btn btn-outline-secondary"
-        // >
-        //     Theme
-        // </button>
         <section className="hstack gap-2">
-            {/* <button className="btn btn-link nav-link py-2 px-0 px-lg-2 dropdown-toggle d-flex align-items-center" id="bd-theme" type="button" aria-expanded="false" data-bs-toggle="dropdown" data-bs-display="static" aria-label="Toggle theme (dark)">
-                <svg className="bi my-1 theme-icon-active"><use href="#moon-stars-fill"></use></svg>
-                <span className="d-lg-none ms-2" id="bd-theme-text">Toggle theme</span>
-            </button> */}
-            {/* <ul className="dropdown-menu dropdown-menu-end" aria-labelledby="bd-theme-text"> */}
-                <button type="button" className="btn" data-bs-theme-value="light">
-                    Light
-                </button>
-                <button type="button" className="btn" data-bs-theme-value="dark">
-                    Dark
-                </button>
-                {/* <li>
-                <button type="button" className="dropdown-item d-flex align-items-center" data-bs-theme-value="auto" aria-pressed="false">
-                    <svg className="bi me-2 opacity-50 theme-icon"><use href="#circle-half"></use></svg>
-                        Auto
-                    <svg className="bi ms-auto d-none"><use href="#check2"></use></svg>
-                </button>
-                </li> */}
-            {/* </ul> */}
+            <button type="button" className="btn" data-bs-theme-value="light">
+                Light
+            </button>
+            <button type="button" className="btn" data-bs-theme-value="dark">
+                Dark
+            </button>
         </section>
     )
 }
